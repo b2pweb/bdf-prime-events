edb9b420327d16886b190e3fad16dfd81ef99462
Vincent <vquatrevieux@b2pweb.com>
2021-07-01 15:56:50 +0200

    Fix MySQL install

--- .github/run-mysql.sh
+++ .github/run-mysql.sh
@@ -18,3 +18,12 @@ do
   sleep 3
   mysql -e 'select version()' && break
 done
+
+mysql -e "create database test"
+mysql -e "create user test_events IDENTIFIED BY 'password'"
+mysql -e "grant all privileges on test.* to test_events"
+mysql -e "grant replication slave, replication client on *.* to test_events"
+mysql -e "create database other"
+mysql -e "create user other_user IDENTIFIED BY 'other_pass'"
+mysql -e "grant all privileges on other.* to other_user"
+mysql -e "grant replication slave, replication client on *.* to other_user"
