5fc526196b119826b68dad705683900d642c9714
Vincent <vquatrevieux@b2pweb.com>
2021-07-01 15:46:03 +0200

    Use script for install mysql

---
+++ .github/run-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mysql() {
+  docker exec mysql mysql "${@}"
+}
+
+docker run -p 0.0.0.0:3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_ROOT_HOST=% --name=mysql -d mysql:5.7 \
+mysqld \
+  --datadir=/var/lib/mysql \
+  --user=mysql \
+  --server-id=1 \
+  --log-bin=/var/lib/mysql/mysql-bin.log \
+  --binlog-format=row \
+  --max_allowed_packet=64M
